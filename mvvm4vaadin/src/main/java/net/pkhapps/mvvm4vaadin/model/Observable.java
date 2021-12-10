2e7e1d63b1fa19f238df46db1d402c643eec9f32
Petter Holmström <petter.holmstrom@outlook.com>
2021-12-10 20:36:26 +0200

    Add JavaDocs to Observable.

--- mvvm4vaadin/src/main/java/net/pkhapps/mvvm4vaadin/model/Observable.java
+++ mvvm4vaadin/src/main/java/net/pkhapps/mvvm4vaadin/model/Observable.java
@@ -21,17 +21,58 @@ import com.vaadin.flow.shared.Registration;
 
 import java.io.Serializable;
 
+/**
+ * Base interface for any object that can be observed by other objects.
+ *
+ * @param <EVENT> the type of event fired by the observable object.
+ */
 public interface Observable<EVENT> extends Serializable {
 
+    /**
+     * Registers the given {@code listener} with this observable object using a strong reference. The listener will
+     * receive events until explicitly de-registered using the returned {@link Registration} handle. The observable will
+     * fire an initial event to the listener upon registration so that it can initialize itself. This is the same as
+     * calling {@link #addListener(SerializableConsumer, boolean) addListener(listener, true)}.
+     *
+     * @param listener the listener to register.
+     * @return a {@link Registration} handle for de-registering the listener when no longer needed.
+     */
     default Registration addListener(SerializableConsumer<? super EVENT> listener) {
         return addListener(listener, true);
     }
 
+    /**
+     * Registers the given {@code listener} with this observable object using a strong reference. The listener will
+     * receive events until explicitly de-registered using the returned {@link Registration} handle.
+     *
+     * @param listener         the listener to register.
+     * @param fireInitialEvent true to fire an initial event to the listener upon registration, false to wait for the
+     *                         first real event before notifying the listener.
+     * @return a {@link Registration} handle for de-registering the listener when no longer needed.
+     */
     Registration addListener(SerializableConsumer<? super EVENT> listener, boolean fireInitialEvent);
 
+    /**
+     * Registers the given {@code listener} with this observable object using a weak reference. The listener will
+     * receive events until it has been garbage collected. It is up to the caller to make sure the listener is not
+     * garbage collected too early. The observable will fire an initial event to the listener upon registration so that
+     * it can initialize itself. This is the same as calling {@link #addWeakListener(SerializableConsumer, boolean)
+     * addWeakListener(listener, true)}.
+     *
+     * @param listener the listener to register.
+     */
     default void addWeakListener(SerializableConsumer<? super EVENT> listener) {
         addWeakListener(listener, true);
     }
 
+    /**
+     * Registers the given {@code listener} with this observable object using a weak reference. The listener will
+     * receive events until it has been garbage collected. It is up to the caller to make sure the listener is not
+     * garbage collected too early.
+     *
+     * @param listener         the listener to register.
+     * @param fireInitialEvent true to fire an initial event to the listener upon registration, false to wait for the
+     *                         first real event before notifying the listener.
+     */
     void addWeakListener(SerializableConsumer<? super EVENT> listener, boolean fireInitialEvent);
 }
