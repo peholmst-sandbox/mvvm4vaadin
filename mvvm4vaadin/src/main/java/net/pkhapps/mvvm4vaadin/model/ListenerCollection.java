462b177c24ee50049ca32f5cbac5465315769a1e
Petter Holmström <petter.holmstrom@outlook.com>
2021-12-12 14:20:43 +0200

    Add JavaDocs to ListenerCollection.

--- mvvm4vaadin/src/main/java/net/pkhapps/mvvm4vaadin/model/ListenerCollection.java
+++ mvvm4vaadin/src/main/java/net/pkhapps/mvvm4vaadin/model/ListenerCollection.java
@@ -27,17 +27,37 @@ import java.util.WeakHashMap;
 
 import static java.util.Objects.requireNonNull;
 
+/**
+ * A collection of listeners that can be added using both strong and weak references. This class is intended for
+ * internal use only and is not thread safe.
+ *
+ * @param <EVENT> the type of event that can be sent to the listeners.
+ */
 class ListenerCollection<EVENT> implements Serializable {
 
+
     @SuppressWarnings({"FieldCanBeLocal", "unused"})
-    private final Serializable owner; // Needed to prevent premature garbage collection of the ListenerCollection
+    // Storing this reference is needed to prevent premature garbage collection of the owner of the ListenerCollection.
+    // This can happen when e.g. mapping an ObservableValue to another ObservableValue and no reference is stored to the
+    // mapped value itself, even though observers have been added to it.
+    private final Serializable owner;
     private Set<SerializableConsumer<? super EVENT>> strongListeners;
     private Map<SerializableConsumer<? super EVENT>, Void> weakListeners;
 
+    /**
+     * Creates a new {@code ListenerCollection}.
+     *
+     * @param owner the object that owns the collection.
+     */
     ListenerCollection(Serializable owner) {
         this.owner = owner;
     }
 
+    /**
+     * Fires the given event to all registered listeners.
+     *
+     * @param event the event to fire.
+     */
     void fireEvent(EVENT event) {
         requireNonNull(event, "event must not be null");
         Set<SerializableConsumer<? super EVENT>> listeners = new HashSet<>();
@@ -50,6 +70,12 @@ class ListenerCollection<EVENT> implements Serializable {
         listeners.forEach(listener -> listener.accept(event));
     }
 
+    /**
+     * Adds the given {@code listener} to this collection using a strong reference.
+     *
+     * @param listener the listener to add.
+     * @return a registration handle for removing the listener when no longer needed.
+     */
     Registration addListener(SerializableConsumer<? super EVENT> listener) {
         requireNonNull(listener, "listener must not be null");
         if (strongListeners == null) {
@@ -59,6 +85,12 @@ class ListenerCollection<EVENT> implements Serializable {
         return () -> strongListeners.remove(listener);
     }
 
+    /**
+     * Adds the given {@code listener} to this collection using a weak reference. The listener will become unregistered
+     * when it is garbage collected.
+     *
+     * @param listener the listener to add.
+     */
     void addWeakListener(SerializableConsumer<? super EVENT> listener) {
         requireNonNull(listener, "listener must not be null");
         if (weakListeners == null) {
@@ -67,6 +99,11 @@ class ListenerCollection<EVENT> implements Serializable {
         weakListeners.put(listener, null);
     }
 
+    /**
+     * Returns whether the collection currently contains any listeners (registered using strong or weak references).
+     *
+     * @return true if there is at least one listener, false otherwise.
+     */
     boolean hasListeners() {
         return (strongListeners != null && strongListeners.size() > 0) || (weakListeners != null && weakListeners.size() > 0);
     }
